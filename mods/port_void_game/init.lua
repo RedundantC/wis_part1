82869a6743369191f44281add349dbe8f2e60aa6
id10tSandwich <50457370+RedundantC@users.noreply.github.com>
2023-12-20 11:45:09 -0500

    Media-Pre-Approval-1.1

--- mods/port_void_game/init.lua
+++
@@ -1,5 +0,0 @@
-local modpath = minetest.get_modpath(minetest.get_current_modname())
-local dofileifexists = greglib.dofileifexists
-greglib.dofileifexists(modpath.."/void_essential.lua")
-greglib.dofileifexists(modpath.."/void_hand.lua")
-
